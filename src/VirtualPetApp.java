0152e6eb55f2c59a24172f040aebfc2f88a9be70
Tiffany <tiffany.chuang2@gmail.com>
2017-10-06 10:44:47 -0400

    first commit

---
+++ src/VirtualPetApp.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class VirtualPetApp {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
